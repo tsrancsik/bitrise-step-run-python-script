51f337d40fc98c9bfdf70b18d656c03d6c2bfd1c
Tamás Srancsik <tamas.srancsik@bitrise.io>
2019-05-02 17:11:05 +0200

    added database variables

--- step.sh
+++ step.sh
@@ -2,4 +2,4 @@
 set -ex
 
 echo "Running python app: ${python_application}"
-python ${python_application}
+python3 ${python_application} ${db_host} ${db_port} ${db_username} ${db_password} ${db_name} ${db_sslmode} || error_exit "Python file not found."
